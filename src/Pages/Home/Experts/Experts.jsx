3e463920fbcc8ba44a2154c430c5f4243a8188f6
kiisi <kiisifelix06@gmail.com>
2022-11-04 15:38:54 +0100

    papersowl responsive

---
+++ src/Pages/Home/Experts/Experts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Experts.scss'
+
+const Experts = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Experts
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from '../../components/major/Navbar/Navbar'
+import Experts from './Experts/Experts'
 import Hero from './Hero/Hero'
+import Papersowl from './Papersowl/Papersowl'
 import Ratings from './Ratings/Ratings'
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Ratings/>
+      <Navbar />
+      <Hero />
+      <Ratings />
+      <Papersowl />
+      <Experts />
     </>
   )
 }
