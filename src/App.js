c037494de7b3449f49bac3d046188cb8b5458b4d
kiisi <kiisifelix06@gmail.com>
2022-11-04 12:18:15 +0100

    Added Hero section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
-import './App.css'
+import './App.scss'
 const App = () => {
   return (
     <div className="app">
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import './Hero.scss'
+import hero_image from '../../../assets/pic-min.png'
 const Hero = () => {
   return (
-    <div className="max-wrapper">
-      
+    <div className="hero-wrapper grid-wrapper ">
+      <div className="hero max-wrapper">
+        <div className="hero-content">
+          <h1>Drowning in tons of <span>assignments</span>? </h1>
+          <h1>We are ready to give you a hand!</h1>
+          <div className="btn-large hero-btn">GET HELP NOW</div>
+        </div>
+        <div className="hero-img">
+          <img src={hero_image} alt="Hero Image"/>
+        </div>
+      </div>
     </div>
   )
 }
