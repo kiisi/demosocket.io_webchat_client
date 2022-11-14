c08ca6875e8b855ed76298bff11445604f62dff9
kiisi <kiisifelix06@gmail.com>
2022-11-14 02:16:42 +0100

    Updates

--- src/components/minor/Button/Fab/Fab.jsx
+++ src/components/minor/Button/Fab/Fab.jsx
@@ -1,30 +1,13 @@
-import React,{useEffect, useRef} from 'react'
+import React from 'react'
 import './Fab.scss'
 
 const Fab = () => {
 
-    const fabRef = useRef()
-   useEffect(()=>{
-        const body = document.querySelector("body")
-        const fab = fabRef.current
-        body.onscroll = () =>{
-            if(window.scrollY > 400){
-                fab.classList.add("fab-show")
-            }else{
-                fab.classList.remove("fab-show")
-            }
-        }
-   })
-   const scrollHandler = () =>{
-    window.scroll({
-        top:0,
-        behavior:'smooth'
-    })
-   }
+
     return (
-        <div className="fab"ref={fabRef} onClick={scrollHandler}>
-            <span className="material-icons">expand_less</span>
-        </div>
+        <a className="fab" href="https://api.whatsapp.com/send?phone=2349155958012&text=Welcome%20%0AMy%20name%20is%20______" target="_blank" rel="noreferrer">
+            <span className="material-icons">chat</span>
+        </a>
     )
 }
 
